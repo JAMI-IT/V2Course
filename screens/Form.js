3debcba6064872d3ed9d499b0c4d357b937a7f02
Shahjahan62 <103955140+JAMI-IT@users.noreply.github.com>
2022-09-06 15:37:11 +0500

    Finalfinal

--- screens/Form.js
+++ screens/Form.js
@@ -1,64 +1,74 @@
-import { View, Text ,TextInput,StyleSheet,TouchableOpacity, Alert,FlatList} from 'react-native'
+import { View, Text ,TextInput,StyleSheet,TouchableOpacity, Alert,FlatList,Button} from 'react-native'
 import React, { useState, useCallback, useEffect } from 'react';
 import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Form() {
 
-    const [FName, SetFname] = useState('');
+  const [FName, SetFname] = useState('');
   const [LName, SetLName] = useState('');
+  const [info, setInfo] = useState([]);
+  const [indexvalue, setindexvalue] = useState();
+  const [operation, setoperation] = useState('Add');
 
-
-const [info, setInfo] = useState([{}]);
-    const handleFullName = useCallback(() => {
-        if (FName==false) {
-            Alert.alert('Please Entert The First Name')
-        }else if (LName==false) {
-            Alert.alert('Please Entetr the Last Name')
-        }
-        else {
-          let names={firstname:FName,lastname:LName}
-          setInfo(current => [...current, names]);
-         
-          console.log(info)
-        }
-    },[FName,LName])
-
-  
-  const clearstate = () => {
-      console.log('DOn Clear');
-      SetFname('');
-      SetLName('');
-  }
-  
-  const udatedata = (index) => {
-    const ind = info.indexOf(index);
-      <View style={{backgroundColor:'red',height:500,width:500}}>
-        <Text>Full Name :{info[ind]}</Text>
-    </View>;
-        console.log(info[ind]);
-
-        console.log('fosnfafsdhfdgsakh');
+  const handleFullName = useCallback(() => {
     
- }
-
-    const handleDelete=useCallback((index) => {        
-      let ind = info.indexOf(index)
-      if (ind > 0) {
-        console.log('DON');
-        info.splice(ind, 1);
-        {
-          clearstate();
-        }
+    if (FName == false) {
+      Alert.alert('Please Entert The First Name')
+    } else if (LName == false) {
+      Alert.alert('Please Entetr the Last Name')
+    }
+      else if (operation=='Update') {
+        
+    setInfo(values => values.map((value, i) => (i === indexvalue ? { firstname: FName, lastname: LName } : value)));
+      setoperation('Add')
+       {
+         clearstate();
+       }
       }
-      },
-      [info],
-  )
+    else if (operation=='Add') {
+      setInfo(current => [...current,{ firstname: FName, lastname: LName }]);
+      {
+        clearstate();
+      }
+    }
+
+  }, [FName, LName])
   
-  useEffect(() => {
-   { clearstate()}
-  },[])
-    
+  const handleDelete = useCallback((index) => {
+    let ind = info.indexOf(index)
+    if (ind >= 0) {
+        {
+          updatetext(index);
+        }
+      info.splice(ind, 1);
+    }
+  }, [info,FName,LName])
+
+  const buttonpress = () => {
+    if (operation == true) {
+      console.log(operation);
+    }
+  }
+
+  const handleUpdate = index => {
+    {
+      updatetext(index);
+    }
+    setindexvalue(info.indexOf(index))
+    setoperation('Update');
+  };
+   const updatetext = index => {
+     SetFname(index.firstname);
+     SetLName(index.lastname);
+   };
+
+   const clearstate = () => {
+     console.log('Don Clear');
+     SetFname('');
+     SetLName('');
+   }; 
   return (
     <SafeAreaView>
       <View style={styles.boxtext}>
@@ -80,7 +90,8 @@ const [info, setInfo] = useState([{}]);
             placeholder="Last Name"
           />
         </View>
-</View>
+      </View>
+
       <View
         style={{
           flexDirection: 'row',
@@ -90,13 +101,7 @@ const [info, setInfo] = useState([{}]);
         }}
       >
         <TouchableOpacity style={styles.Buttonpress} onPress={handleFullName}>
-          <Text>Added</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Buttonpress} onPress={{}}>
-          <Text>Update</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Buttonpress} onPress={{}}>
-          <Text>Delete</Text>
+          <Text>{operation}</Text>
         </TouchableOpacity>
       </View>
       <View>
@@ -106,7 +111,14 @@ const [info, setInfo] = useState([{}]);
           renderItem={({ item }) => (
             console.log(info),
             (
-              <View style={{ flexDirection: 'row' }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  marginHorizontal: 10,
+                }}
+              >
                 <TouchableOpacity onPress={() => handleDelete(item)}>
                   <Text
                     style={{
@@ -122,14 +134,24 @@ const [info, setInfo] = useState([{}]);
                   </Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                  style={styles.Fname}
-                  onPress={() => udatedata(item)}
+                  onPress={() => handleUpdate(item)}
+                  style={{
+                    backgroundColor: '#0000FF',
+                    height: 40,
+                    width: 40,
+                    padding: 8,
+                    borderRadius: 50,
+                    margin: 5,
+                  }}
                 >
                   <Text>
-                    Full Name :{item.firstname}
-                    {item.lastname}
+                    <FontAwesome name="exchange" size={24} color="white" />
                   </Text>
                 </TouchableOpacity>
+                <Text style={styles.Fname}>
+                  Full Name :{item.firstname}
+                  {item.lastname}
+                </Text>
               </View>
             )
           )}
@@ -138,7 +160,6 @@ const [info, setInfo] = useState([{}]);
     </SafeAreaView>
   );
 }
-
 
 const styles = StyleSheet.create({
   boxtext: {
@@ -164,14 +185,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
     Fname: {
-    alignSelf: 'center',
     height: 40,
-    width: "80%",
+    width: "65%",
     backgroundColor: 'teal',
     borderRadius: 5,
-      margin: 5,
-      color: 'white',
-        justifyContent:'center',
-      padding:4,
+      padding: 4,
+      color:'black'
   },
 });
